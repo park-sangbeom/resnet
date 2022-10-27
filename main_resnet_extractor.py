d78dd5c28a851c7e3060616bfee7eb027df40a1a
Beom0611 <psb5657@naver.com>
2022-10-27 22:47:53 +0900

    resnet 1028 ver.

--- main_resnet_extractor.py
+++ main_resnet_extractor.py
@@ -107,9 +107,9 @@ for hparams in RunBuilder.get_runs_from_params(param_names, parameters):
             for i in range(5):
                 axs[0][i].matshow(np.reshape(val_images[i, :], (96,192)))
                 axs[1][i].matshow(np.reshape(val_preds[i, :], (96,192)))
-            plt.savefig("data/resnet/resnet1027_eval{}.png".format(epoch+1))
-            torch.save(ae.encoder.state_dict(), 'weights/resnet1027/resnet_encoder{}steps.pth'.format(epoch+1))
-            torch.save(ae.decoder.state_dict(), 'weights/resnet1027/resnet_decoder{}steps.pth'.format(epoch+1))
+            plt.savefig("data/resnet/resnet1028_eval{}.png".format(epoch+1))
+            torch.save(ae.encoder.state_dict(), 'weights/resnet1028/resnet_encoder{}steps.pth'.format(epoch+1))
+            torch.save(ae.decoder.state_dict(), 'weights/resnet1028/resnet_decoder{}steps.pth'.format(epoch+1))
 
     # m.end_run()
     print("Model has finished training.\n")
--- model/resnet_for_extractor.py
+++ model/resnet_for_extractor.py
@@ -50,8 +50,9 @@ class Encoder(nn.Module):
         self.BN = nn.BatchNorm2d(16)
         self.rb1 = ResBlock(16, 16, 3, 2, 1, 'encode')
         self.rb2 = ResBlock(16, 32, 3, 2, 1, 'encode') 
+        self.rb3 = ResBlock(32, 48, 3, 2, 1, 'encode') 
         self.flatten = nn.Flatten()
-        self.lin1 = nn.Linear(32*24*48, 1024)
+        self.lin1 = nn.Linear(48*12*24, 1024)
         self.lin2 = nn.Linear(1024, 32)
         self.relu = nn.ReLU()
 
@@ -59,6 +60,7 @@ class Encoder(nn.Module):
         init_conv = self.relu(self.BN(self.init_conv(inputs)))
         out = self.rb1(init_conv)
         out = self.rb2(out)
+        out = self.rb3(out)
         out = self.flatten(out)
         out = self.relu(self.lin1(out))
         out = self.relu(self.lin2(out))
@@ -71,9 +73,11 @@ class Decoder(nn.Module):
     
     def __init__(self):
         super(Decoder, self).__init__()
-        self.rb1 = ResBlock(32, 16, 3, 2, 1, 'decode') # 16 16 16
-        self.rb2 = ResBlock(16, 16, 2, 2, 0, 'decode') # 16 32 32
-        self.de_lin1 = nn.Linear(1024, 32*24*48)
+        self.rb1 = ResBlock(48, 32, 5, 2, 1, 'decode') # 16 16 16
+        self.rb2 = ResBlock(32, 16, 2, 2, 1, 'decode') # 16 32 32
+        self.rb3 = ResBlock(16, 16, 2, 2, 1, 'decode') # 16 32 32
+
+        self.de_lin1 = nn.Linear(1024, 48*12*24)
         self.de_lin2 = nn.Linear(32, 1024)
         self.out_conv = nn.ConvTranspose2d(16, 1, 3, 1, 0) # 3 32 32
         self.tanh = nn.Tanh()
@@ -82,9 +86,10 @@ class Decoder(nn.Module):
     def forward(self, inputs):
         out = self.relu(self.de_lin2(inputs))
         out = self.relu(self.de_lin1(out))
-        out = out.view(-1, 32, 24, 48)
+        out = out.view(-1, 48, 12, 24)
         out = self.rb1(out)
         out = self.rb2(out)
+        out = self.rb3(out)
         out = self.out_conv(out)
         out = self.tanh(out)
         return out
